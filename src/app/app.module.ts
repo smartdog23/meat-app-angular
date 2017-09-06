55d010dcabe7d6f17c52c8479e0ada0b24c6eb8c
Valdir P. Lourenco <valdir2310@yahoo.com.br>
2017-09-05 23:21:08 -0300

    component input - part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,8 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ShoppingCartService } from './shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component'
+import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component'
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { OrderComponent } from './order/order.component'
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
